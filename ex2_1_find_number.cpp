3d4938a86dbd1192589b6242b2852b2e8bcda10f
Lorite <alejandrolorite@opendeusto.es>
2020-09-15 11:02:54 +0200

    First commit

---
+++ ex2_1_find_number.cpp
@@ -0,0 +1,10 @@
+#include "ex2_1_find_number.h"
+
+Ex2_1_Find_number::Ex2_1_Find_number()
+{
+
+}
+
+int main() {
+    return 0;
+}
---
+++ ex2_1_find_number.h
@@ -0,0 +1,11 @@
+#ifndef EX2_1_FIND_NUMBER_H
+#define EX2_1_FIND_NUMBER_H
+
+
+class Ex2_1_Find_number
+{
+public:
+    Ex2_1_Find_number();
+};
+
+#endif // EX2_1_FIND_NUMBER_H
