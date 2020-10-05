5cd4d58d4caec424cda599077253666e73a58c1a
Lorite <alejandrolorite@opendeusto.es>
2020-10-05 09:27:48 +0200

    New Algorithm 1D function minimization

---
+++ Algorithm 1D function minimization/Alg1Dfuncmin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+
+double fexpression(double x)
+{
+   return (5 * x * x) - 5 * x + 3; //f = 5x² - 5x - c;
+}
+
+double fexpressionDer(double x, double eps)
+{
+   return (fexpression(x+eps) - fexpression(x))/eps;
+}
+
+double Alg1Dfuncmin(double startingX, int iterMax, double gainLambda, double minGradinentGm) {
+    double minX = startingX;
+    int iter = 0;
+    double gradG = 2 * minGradinentGm;
+    while((iter < iterMax) && (abs(gradG) > minGradinentGm)) {
+        gradG = fexpressionDer(minX, 0.00001);
+        minX -= gainLambda*gradG;
+        //std::cout << minX << std::endl;
+        iter++;
+    }
+    return minX;
+}
+
+int main() {
+    std::cout << "The minimum is " << Alg1Dfuncmin(0, 100000, 0.0001, 0.000001) << ".";
+	return 0;
+} 
---
+++ Algorithm 1D function minimization/build/CMakeFiles/Alg1Dfuncmin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Alg1Dfuncmin"
+  "Alg1Dfuncmin.pdb"
+  "CMakeFiles/Alg1Dfuncmin.dir/Alg1Dfuncmin.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Alg1Dfuncmin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
